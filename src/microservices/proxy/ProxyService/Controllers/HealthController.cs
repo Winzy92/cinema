e7a249a0f4847b416402d3e7bdc9d302b54258c3
Дмитрий <kuznetsov.dmitr22@yandex.ru>
2025-11-26 23:08:31 +0300

    fix path

--- src/microservices/proxy/ProxyService/Controllers/HealthController.cs
+++ src/microservices/proxy/ProxyService/Controllers/HealthController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace ProxyService.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/health")]
 public class HealthController : ControllerBase
 {
     [HttpGet]
